87ba5b9838330f76136f9f2a75d636a65236f719
KayraAkin <kayrakin11@hotmail.com>
2024-02-17 18:25:44 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Business.Concrete;
+using Entities.Concrete;
+
+
+
+Person person1 = new Person();
+person1.FirstName = "Mehmet Kayra";
+person1.LastName = "Akın";
+person1.DateOfBirthYear = 1999;
+person1.NationalIdentity = 123;
+
+
+
+PttManager pttManager = new PttManager( new PersonManager());
+pttManager.GiveMask(person1);
